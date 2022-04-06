bebf4de75fc5a081f29503c2228ac0de561e9219
Maan <96055301+Maan-py@users.noreply.github.com>
2022-04-06 11:05:31 +0700

    Add files via upload

---
+++ manipulatingComplexObjects.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "123",
+      "title": "123",
+      "release_year": 123,
+      "formats": [
+        "1",
+        "2",
+        "3"
+      ]
+    }
+  ];
